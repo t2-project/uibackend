0ae8b33e0438e27f4c0b77e5147f4ce4cca6e330
delvh <leon@kske.dev>
2022-08-19 15:48:01 +0200

    Remove redundant class scan

--- src/main/java/de/unistuttgart/t2/uibackend/UIBackendApplication.java
+++ src/main/java/de/unistuttgart/t2/uibackend/UIBackendApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-import de.unistuttgart.t2.common.BaseScan;
 import io.swagger.v3.oas.models.*;
 import io.swagger.v3.oas.models.info.Info;
 
@@ -15,7 +14,7 @@ import io.swagger.v3.oas.models.info.Info;
  *
  * @author maumau
  */
-@SpringBootApplication(scanBasePackageClasses = { BaseScan.class, UIBackendApplication.class })
+@SpringBootApplication
 public class UIBackendApplication {
 
     @Value("${t2.orchestrator.url}")
